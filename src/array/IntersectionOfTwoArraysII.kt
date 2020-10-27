3351b86cafc95537923194967c2300f83a5062ac
Abed Murad <abed.murad@protonmail.com>
2020-10-27 15:40:26 +0200

    impl intersect() & intersectSorted()

--- src/array/IntersectionOfTwoArraysII.kt
+++ src/array/IntersectionOfTwoArraysII.kt
@@ -1,8 +1,14 @@
 package array
 
+import kotlin.collections.set
+
+
 fun main() {
     intersect(intArrayOf(1, 2, 2, 1), intArrayOf(2, 2))
     intersect(intArrayOf(4, 9, 5), intArrayOf(9, 4, 9, 8, 4))
+
+    intersectSorted(intArrayOf(1, 2, 2, 1), intArrayOf(2, 2))
+    intersectSorted(intArrayOf(4, 9, 5), intArrayOf(9, 4, 9, 8, 4))
 }
 
 // Time O(N+M), Space = O(N) for the HashMap
@@ -21,4 +27,38 @@ fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
         }
     }
     return list.toIntArray()
+}
+
+// if the arrays were sorted
+fun intersectSorted(nums1: IntArray, nums2: IntArray): IntArray {
+
+    nums1.sort()
+    nums2.sort()
+
+    val n = nums1.size
+    val m = nums2.size
+    var i = 0
+    var j = 0
+
+    val result = mutableListOf<Int>()
+
+    while (i < n && j < m) {
+        val a = nums1[i]
+        val b = nums2[j]
+        when {
+            (a == b) -> {
+                result.add(a)
+                i++
+                j++
+            }
+            (a < b) -> {
+                i++
+            }
+            else -> {
+                j++
+            }
+        }
+    }
+    return result.toIntArray()
+
 }
