9335961e498dbcb46996b1407d20d2d23ade0b03
Abed Murad <abed.murad@protonmail.com>
2020-10-27 13:42:24 +0200

    Initial commit

---
+++ src/array/RotateArray.kt
@@ -0,0 +1,27 @@
+package array
+
+fun main() {
+    rotate(intArrayOf(1,2), 3)
+}
+
+fun rotate(nums: IntArray, k: Int) {
+    var myK = k % nums.size
+    reverse(nums, 0, nums.size - 1)
+    reverse(nums, 0, myK - 1)
+    reverse(nums, myK, nums.size - 1)
+}
+
+fun reverse(nums: IntArray, start: Int, end: Int) {
+    @Suppress("NAME_SHADOWING")
+    var start = start
+
+    @Suppress("NAME_SHADOWING")
+    var end = end
+    while (start < end) {
+        val temp = nums[start]
+        nums[start] = nums[end]
+        nums[end] = temp
+        start++
+        end--
+    }
+}
