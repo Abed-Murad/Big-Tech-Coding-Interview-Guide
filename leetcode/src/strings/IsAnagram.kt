002b1bc04338cb7e7cedc711ecb4ae91ee193510
Abed Murad <abed.murad@protonmail.com>
2020-10-29 15:33:36 +0300

    Impl isAnagram().

---
+++ leetcode/src/strings/IsAnagram.kt
@@ -0,0 +1,47 @@
+package strings
+
+fun main() {
+    isAnagram("abed", "deba")
+    isAnagram("anagram", "nagaram")
+    isAnagram("rat", "car")
+}
+
+fun isAnagram(s: String, t: String): Boolean {
+    if (s.length != t.length) return false
+
+    val table = IntArray(26)
+
+    for (char in s) {
+        table[char - 'a']++
+    }
+
+    for (char in t) {
+        table[char - 'a']--
+    }
+
+    return table.all { it == 0 }
+}
+
+fun isAnagramWithHashMap(s: String, t: String): Boolean {
+    if (s.length != t.length) return false
+    val firstMap = HashMap<Char, Int>()
+    val secondMap = HashMap<Char, Int>()
+
+    for (char in s) {
+        val count = firstMap.getOrDefault(char, 0)
+        firstMap[char] = count + 1
+    }
+
+    for (char in t) {
+        val count = secondMap.getOrDefault(char, 0)
+        secondMap[char] = count + 1
+    }
+    if (firstMap.size != secondMap.size) return false
+    for (char in t) {
+        if (secondMap[char] != firstMap[char]) {
+            return false
+        }
+    }
+    return true
+
+}
