ed51ca7f978e5bd3939c051937c3927a1018da12
Abed Murad <abed.murad@protonmail.com>
2020-11-05 23:57:53 +0200

    Add comment to hasCycle().

--- leetcode/src/linkidlist/CycleLinkedList.kt
+++ leetcode/src/linkidlist/CycleLinkedList.kt
@@ -7,7 +7,7 @@ fun main() {
 fun hasCycle(head: ListNode?): Boolean {
     var walker = head
     var runner = head
-    // it will not reach a null tail if it has cycle
+    // it will not reach a null tail if it has cycle, if it did, its not a CycleLinkedList.
     while (walker?.next != null && runner?.next != null) {
         runner = runner.next!!.next!!
         walker = walker.next
