4e98caecd01f9ccc4d686942d736dd98762e8b12
Abed Murad <abed.murad@protonmail.com>
2020-11-06 00:15:57 +0200

    Update hasCycle()

--- leetcode/src/linkidlist/CycleLinkedList.kt
+++ leetcode/src/linkidlist/CycleLinkedList.kt
@@ -5,13 +5,16 @@ fun main() {
 }
 
 fun hasCycle(head: ListNode?): Boolean {
+    head?.next ?: return false
     var walker = head
     var runner = head
+
     // it will not reach a null tail if it has cycle, if it did, its not a CycleLinkedList.
-    while (walker?.next != null && runner?.next != null) {
-        runner = runner.next!!.next!!
-        walker = walker.next
-        if (walker == runner) return true
+    while (runner?.next != null) {
+        runner = runner.next?.next
+        walker = walker?.next
+        if (runner == walker) return true
     }
     return false
 }
+
