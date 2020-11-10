981fe9104e3b12dbeb0138071ed780f84645b316
Abed Murad <abed.murad@protonmail.com>
2020-11-10 14:34:26 +0200

    Impl invertTree().

---
+++ leetcode/src/trees/InvertBinaryTree.kt
@@ -0,0 +1,17 @@
+package trees
+
+import TreeNode
+
+fun main() {
+
+}
+
+fun invertTree(root: TreeNode?): TreeNode? {
+    if (root == null) return null
+    val temp = root.left
+    root.left = root.right
+    root.right = temp
+    invertTree(root.left)
+    invertTree(root.right)
+    return root
+}
