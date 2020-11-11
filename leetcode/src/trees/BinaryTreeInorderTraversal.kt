88cfa4392af1b82ac37dac18fceffc8433dba530
Abed Murad <abed.murad@protonmail.com>
2020-11-11 20:24:11 +0200

    Impl inorderTraversal() but using Iterating and a stack. Ride by Twenty One Pilots is a great song! 8.5/10.

--- leetcode/src/trees/BinaryTreeInorderTraversal.kt
+++ leetcode/src/trees/BinaryTreeInorderTraversal.kt
@@ -1,9 +1,28 @@
 package trees
 
 import TreeNode
+import array.matreices.rotate
 import java.util.*
 
 fun main() {
+    val one = TreeNode(1)
+    val two = TreeNode(2)
+    val three = TreeNode(3)
+    val four = TreeNode(4)
+    val five = TreeNode(5)
+    val six = TreeNode(6)
+    val seven = TreeNode(7)
+
+    four.left = two
+    four.right = six
+
+    two.left = one
+    two.right = three
+
+    six.left = five
+    six.right = seven
+
+    inorderTraversalIterating(four)
 
 }
 
@@ -13,9 +32,35 @@ fun inorderTraversal(root: TreeNode?): List<Int> {
     return list
 }
 
+// Time complexity = O(n)
+// Space complexity = O(n)
 fun inorder(node: TreeNode?, list: MutableList<Int>) {
     if (node == null) return
     inorder(node.left, list)
     list.add(node.`val`)
     inorder(node.right, list)
 }
+
+
+// Time complexity = O(n)
+// Space complexity = O(n)
+// check out for good illustrations https://leetcode.com/problems/binary-tree-inorder-traversal/solution/
+fun inorderTraversalIterating(root: TreeNode?): List<Int> {
+    val list = mutableListOf<Int>()
+    if (root == null) return list
+    val stack = LinkedList<TreeNode>()
+
+    var current = root
+    while (current != null || stack.isNotEmpty()) {
+        // discover loop to discover if there is any undiscovered nodes
+        while (current != null) {
+            stack.push(current)
+            current = current.left
+        }
+        current = stack.pop() // Adding values to the list to be returned.
+        list.add(current.`val`)
+        current = current.right
+    }
+    return list
+}
+
