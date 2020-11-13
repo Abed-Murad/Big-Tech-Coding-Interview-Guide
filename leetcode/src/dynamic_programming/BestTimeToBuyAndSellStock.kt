6dac9561b5bd323268485705e052772d86759c09
Abed Murad <abed.murad@protonmail.com>
2020-11-13 20:14:49 +0200

    impl maxProfit().

---
+++ leetcode/src/dynamic_programming/BestTimeToBuyAndSellStock.kt
@@ -0,0 +1,18 @@
+package dynamic_programming
+
+fun main() {
+
+}
+
+fun maxProfit(prices: IntArray): Int {
+    var minPrice = Int.MAX_VALUE
+    var maxProfit = 0
+    for (i in prices.indices) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i]
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice
+        }
+    }
+    return maxProfit
+}
