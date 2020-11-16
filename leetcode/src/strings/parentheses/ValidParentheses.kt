a37e88a9806273d3fd8d8e50cb3e42587307ca55
Abed Murad <abed.murad@protonmail.com>
2020-11-17 00:03:14 +0200

    Impl ValidParentheses(). almost done with Leetcode TIQ-EC.

--- leetcode/src/strings/parentheses/ValidParentheses.kt
+++ leetcode/src/strings/parentheses/ValidParentheses.kt
@@ -1,30 +1,35 @@
 package strings.parentheses
 
 import java.util.*
+import kotlin.collections.HashMap
+
+val map = HashMap<Char, Char>()  // Hash table that takes care of the mappings.
 
 fun main() {
+    map[')'] = '('
+    map['}'] = '{'
+    map[']'] = '['
+
     isValid("()[]{}")
 }
 
+
 fun isValid(s: String): Boolean {
-    val stack1 = Stack<Char>()
-    val stack2: Queue<Char> = LinkedList()
+    val stack = Stack<Char>()
 
     for (char in s) {
-        if (char == '(' || char == '[' || char == '{') {
-            stack1.add(char)
-        } else if (char == ')' || char == ']' || char == '}') {
-            if (char == ')') stack2.add('(')
-            if (char == ']') stack2.add('[')
-            if (char == '}') stack2.add('{')
+        // if the current char is a closing bracket.
+        if (map.contains(char)) {
+            // Get the top element of the stack. if the stack is empty, set a dummy value of '#'
+            val top = if (stack.empty()) '#' else stack.pop()
+            // if the mapping for this bracket doesn't match the stack's top element, return false.
+            if (top != map[char]) {
+                return false
+            }
+        } else {
+            // If it was an opening bracket, push to the stack.
+            stack.push(char)
         }
     }
-    if (stack1.size != stack2.size) return false
-
-    for (i in 0 until stack1.size) {
-        if (stack1.pop() != stack2.remove()) {
-            return false
-        }
-    }
-    return true
+    return stack.empty()
 }
