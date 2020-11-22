40f0edcf1720371d48307e6be0775b84165e921f
Abed Murad <abed.murad@protonmail.com>
2020-11-23 01:38:31 +0200

    Impl selectionSort().

---
+++ pure_algorithms/src/sorting/SelectionSort.kt
@@ -0,0 +1,31 @@
+package sorting
+
+fun main() {
+    selectionSort(intArrayOf(3, 5, 8, 4, 1, 9, -2))
+}
+
+fun selectionSort(arr: IntArray): IntArray {
+    var lastIndex = arr.size - 1
+
+    // find the minimum element in the unsorted sub-array[i..n-1]
+    // and swap it with arr[i]
+    for (i in 0..lastIndex) {
+        var min = i
+        for (j in i + 1..lastIndex) {
+            // if arr[j] element is less, then it is the new minimum
+            if (arr[j] < arr[min]) {
+                min = j // update index of min element
+            }
+        }
+        // swap the minimum element in sub-array[i..n-1] with arr[i]
+        swap(arr, min, i)
+    }
+    return arr
+}
+
+
+fun swap(arr: IntArray, i: Int, j: Int) {
+    val tmp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = tmp
+}
