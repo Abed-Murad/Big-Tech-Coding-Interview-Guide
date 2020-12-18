743ed6ae8fea04d36d2fadbc209eee510f383705
Abed Murad <abed.murad@protonmail.com>
2020-12-18 15:06:31 +0200

    Create Graph interface, Impl Graph interface using AdjacencyList.

---
+++ pure_data_structure/src/graph/Graph.kt
@@ -0,0 +1,50 @@
+package graph
+// A Graph is a data structure that captures relationships between objects
+// Types:
+// 1. Weighted Graphs: each edge has a weight
+// 2. Directed Graphs: n edge may only permit traversal in one direction
+// 3. Undirected graphs: a directed graph where all of the edges are bi-directional.
+
+data class Vertex<T>(val index: Int, val data: T)
+
+data class Edge<T>(
+    val source: Vertex<T>,
+    val destination: Vertex<T>,
+    val weight: Double? = null
+)
+
+interface Graph<T> {
+
+    fun createVertex(data: T): Vertex<T>
+
+    fun addDirectedEdge(
+        source: Vertex<T>,
+        destination: Vertex<T>,
+        weight: Double?
+    )
+
+    fun addUndirectedEdge(
+        source: Vertex<T>,
+        destination: Vertex<T>,
+        weight: Double?
+    )
+
+    fun add(
+        edge: EdgeType,
+        source: Vertex<T>,
+        destination: Vertex<T>,
+        weight: Double?
+    )
+
+    fun edges(source: Vertex<T>): ArrayList<Edge<T>>
+
+    fun weight(
+        source: Vertex<T>,
+        destination: Vertex<T>
+    ): Double?
+
+}
+
+enum class EdgeType {
+    DIRECTED, UNDIRECTED
+}
