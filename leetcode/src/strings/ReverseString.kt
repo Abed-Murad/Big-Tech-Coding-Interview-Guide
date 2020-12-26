22ea3d5c8edddb19c2d513921b98ec836749b76e
Abed Murad <abed.murad@protonmail.com>
2020-12-26 14:01:06 +0200

    Update reverseString().

--- leetcode/src/strings/ReverseString.kt
+++ leetcode/src/strings/ReverseString.kt
@@ -1,16 +1,31 @@
 package strings
 
-import array.reverse
-
 fun main() {
     reverseString("123456789".toCharArray())
+    reverseStringRecursively("123456789".toCharArray(), 0, "123456789".length - 1)
 }
 
 fun reverseString(s: CharArray): Unit {
-    for (i in 0 until s.size / 2) {
-        val tmp = s[i]
-        s[i] = s[s.size - 1 - i]
-        s[s.size - 1 - i] = tmp
+    var start = 0
+    var end = s.size - 1
+    while (start < end) {
+        val tmp = s[start]
+        s[start] = s[end]
+        s[end] = tmp
+        start++
+        end--
     }
-    println(s)
+}
+
+// This one is not in-place, recursion uses a recursion stack as an auxiliary data structure.
+// but it's cool to be able to find the recursion solution at first try.
+fun reverseStringRecursively(s: CharArray, start: Int, end: Int) {
+    if (start > end) {
+        println(s)
+        return
+    }
+    val tmp = s[start]
+    s[start] = s[end]
+    s[end] = tmp
+    reverseStringRecursively(s, start + 1, end - 1)
 }
