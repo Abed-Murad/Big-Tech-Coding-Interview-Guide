570673e80385215703387938f07e5addd33a39b8
Abed Murad <abed.murad@protonmail.com>
2020-12-28 15:44:09 +0200

    Impl reverseVowels()

--- leetcode/src/array/ReverseVowelsOfAString.kt
+++ leetcode/src/array/ReverseVowelsOfAString.kt
@@ -1,46 +1,38 @@
 package array
 
-import java.lang.StringBuilder
 
 // English vowels: A, E, I, O, U
 // English consonants: B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Y, Z
 fun reverseVowels(s: String): String {
-    var s = s.toCharArray()
+    val s = s.toCharArray()
     var start = 0
-    var end = s.lastIndex
-
-    var endVowel = -1
-    var startVowel = -1
+    var end = s.size - 1
     while (start < end) {
-        if (startVowel > 0 && endVowel > 0) {
-            val tmp = s[startVowel]
-            s[startVowel] = s[endVowel]
-            s[endVowel] = tmp
-            startVowel = -1
-            endVowel = -1
+        if (s[start].isVowel() && s[end].isVowel()) {
+            s.swap(start++, end--)
         }
-        if (s[start].toLowerCase() in arrayListOf('a', 'e', 'i', 'o', 'u')) {
-            startVowel = start
+        if (!s[start].isVowel()) {
+            start++
         }
-        if (s[end].toLowerCase() in arrayListOf('a', 'e', 'i', 'o', 'u')) {
-            endVowel = end
+        if (!s[end].isVowel()) {
+            end--
         }
-
-        if (startVowel != -1 && endVowel != -1) {
-            val tmp = s[startVowel]
-            s[startVowel] = s[endVowel]
-            s[endVowel] = tmp
-            startVowel = -1
-            endVowel = -1
-        }
-
-        start++
-        end--
     }
     return String(s)
-
 }
 
+private fun Char.isVowel(): Boolean {
+    val c = this.toLowerCase()
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+}
+
+private fun CharArray.swap(i: Int, j: Int) {
+    val tmp = this[i]
+    this[i] = this[j]
+    this[j] = tmp
+}
+
+
 fun main() {
-    println(reverseVowels("ai"))
+    println(reverseVowels("Euston saw I was not Sue."))
 }
