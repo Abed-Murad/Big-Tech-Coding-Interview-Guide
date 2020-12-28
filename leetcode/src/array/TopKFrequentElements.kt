940c823cf54d34f493b4bf69e80eb1c1ed39eabb
Abed Murad <abed.murad@protonmail.com>
2020-12-28 22:05:51 +0200

    Init Heaping around!

--- leetcode/src/array/TopKFrequentElements.kt
+++ leetcode/src/array/TopKFrequentElements.kt
@@ -1,2 +1,17 @@
 package array
 
+fun topKFrequent(nums: IntArray, k: Int): IntArray {
+    val map = HashMap<Int, Int>()
+
+    for (n in nums) {
+        val value = map.getOrDefault(n, 0)
+        map[n] = value + 1
+
+    }
+    map.toSortedMap()
+    return intArrayOf(1)
+}
+
+fun main() {
+    topKFrequent(intArrayOf(2, 1, 1, 2, 2, 3), 2)
+}
---
+++ pure_data_structure/src/heap/Collection.kt
@@ -0,0 +1,5 @@
+package heap
+
+interface Collection<Element> {
+
+}
